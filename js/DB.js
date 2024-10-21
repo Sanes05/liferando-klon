e60cc6af6df8a0f594bf1b50d0f460988367df2a
Sanes05 <kevinmauricejordan2005@gmail.com>
2024-10-21 14:05:56 +0200

    DB Js fertig gemacht

--- js/DB.js
+++ js/DB.js
@@ -1,12 +1,53 @@
 let myDishes = [
-	{
-		name: "Cheeseburger",
-		price: 8.99,
-		description: "Einfach nur lecker",
-	},
-	{
-		name: "Hamburger",
-		price: 7.99,
-		description: "Einfach nur lecker",
-	},
+	[
+		//Burger
+		{
+			name: "Classic Cheese Burger",
+			price: 9.65,
+			description: "mit Rindfleisch, Cheddar, Tomaten, roten Zwiebeln, sauren Gurken, Lolo Bionda und hausgemachtem Dressing",
+		},
+		{
+			name: "Hamburger",
+			price: 8.75,
+			description: "mit Rindfleisch, Tomaten, roten Zwiebeln, sauren Gurken, Lolo Bionda und hausgemachtem Dressing",
+		},
+		{
+			name: "Tex Mex Burger (scharf)",
+			price: 10.45,
+			description: "mit Rindfleisch, Cheddar, Jalapenos, Tomaten, roten Zwiebeln, hausgemachter scharfer Sauce und Dressing",
+		},
+	],
+	[
+		//Burger Menüs
+		{
+			name: "Classic Cheese Burger Menü",
+			price: 16.95,
+			description: "mit Rindfleisch, Cheddar, Tomaten, roten Zwiebeln, sauren Gurken, Lolo Bionda , hausgemachtem Dressing und Pommes Frites",
+		},
+		{
+			name: "Hamburger Menü",
+			price: 15.95,
+			description: "mit Rindfleisch, Tomaten, roten Zwiebeln, sauren Gurken, Lolo Bionda , hausgemachtem Dressing und Pommes Frites",
+		},
+		{
+			name: "Tex Mex Burger Menü (scharf)",
+			price: 17.95,
+			description: "mit Rindfleisch, Cheddar, Jalapenos, Tomaten, roten Zwiebeln, hausgemachter scharfer Sauce und Dressing und Pommes Frites",
+		},
+	],
+	[
+		//Beilagen
+		{
+			name: "Pommes Frites",
+			price: 5.45,
+		},
+		{
+			name: "Süßkartoffel Pommes",
+			price: 6.45,
+		},
+		{
+			name: "Onion Rings",
+			price: 7.45,
+		},
+	],
 ];
