a7b967c049006ee66a4f5805e2cfeff859f2a6b7
Sanes05 <kevinmauricejordan2005@gmail.com>
2024-10-21 10:28:06 +0200

    Fonts Hinzugefügt

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-function onload() {}
+function onload() {
+	loadBasket();
+}
+
+function loadBasket() {
+	let basket = document.getElementById("basket");
+	basket.innerHTML = basketTemplate();
+}
---
+++ js/templates.js
@@ -0,0 +1,7 @@
+const basketTemplate = () => {
+	return `
+        <div class="basket-headline-div">
+            <h2 class="basket-headline">Warenkorb</h2>
+        </div>
+    `;
+};
