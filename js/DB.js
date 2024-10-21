8a2f69c0f289f7558555fc9e06764107fbd046cf
Sanes05 <kevinmauricejordan2005@gmail.com>
2024-10-21 09:46:58 +0200

    css variables

---
+++ js/DB.js
@@ -0,0 +1,12 @@
+let myDishes = [
+	{
+		name: "Cheeseburger",
+		price: 8.99,
+		description: "Einfach nur lecker",
+	},
+	{
+		name: "Hamburger",
+		price: 7.99,
+		description: "Einfach nur lecker",
+	},
+];
