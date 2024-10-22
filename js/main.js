060dc10db908d3751a3ac42d989d63f5da5dd73b
Sanes05 <kevinmauricejordan2005@gmail.com>
2024-10-22 09:41:41 +0200

    code verkürzt

--- js/main.js
+++ js/main.js
@@ -10,13 +10,9 @@ function renderBasket() {
 
 function renderDishes() {
 	let dishes = [document.getElementById("burger"), document.getElementById("burger-menu"), document.getElementById("hot-dog")];
-	myDishes[0].forEach((dish) => {
-		dishes[0].innerHTML += dishTemplate(dish.name, dish.price, dish.description);
-	});
-	myDishes[1].forEach((dish) => {
-		dishes[1].innerHTML += dishTemplate(dish.name, dish.price, dish.description);
-	});
-	myDishes[2].forEach((dish) => {
-		dishes[2].innerHTML += dishTemplate(dish.name, dish.price, dish.description);
+	dishes.forEach((dishElement, index) => {
+		myDishes[index].forEach((dish) => {
+			dishElement.innerHTML += dishTemplate(dish.name, dish.price, dish.description);
+		});
 	});
 }
