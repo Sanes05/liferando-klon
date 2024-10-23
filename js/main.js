ca8de90581c4489082f7945afeb5814bc83297dd
Sanes05 <kevinmauricejordan2005@gmail.com>
2024-10-23 14:01:25 +0200

    add und remove func gemacht geht aber noch nciht

--- js/main.js
+++ js/main.js
@@ -3,16 +3,46 @@ function onload() {
 	renderDishes();
 }
 
-function renderBasket() {
-	let basket = document.getElementById("basket");
-	basket.innerHTML = basketTemplate();
-}
+let basket = [
+	{
+		name: "Classic Cheese Burger",
+		price: 9.65,
+	},
+	{
+		name: "Hamburger",
+		price: 8.65,
+	},
+];
 
 function renderDishes() {
 	let dishes = [document.getElementById("burger"), document.getElementById("burger-menu"), document.getElementById("hot-dog")];
 	dishes.forEach((dishElement, index) => {
 		myDishes[index].forEach((dish) => {
-			dishElement.innerHTML += dishTemplate(dish.name, dish.price, dish.description);
+			const price = dish.price.toString().replace(".", ",");
+			dishElement.innerHTML += dishTemplate(dish.name, price, dish.description);
 		});
 	});
+}
+
+function addToBasket() {}
+
+let ammount = 0;
+
+const addAmmount = () => {
+	ammount++;
+	console.log(ammount);
+};
+
+const removeAmmount = () => {
+	ammount--;
+	console.log(ammount);
+};
+
+function renderBasket() {
+	let basketRef = document.getElementById("basket");
+	for (let basketIndex = 0; basketIndex < basket.length; basketIndex++) {
+		const basketItems = basket[basketIndex];
+		const price = basketItems.price.toString().replace(".", ",");
+		basketRef.innerHTML += basketTemplate(basketItems.name, price, ammount);
+	}
 }
--- js/templates.js
+++ js/templates.js
@@ -1,8 +1,19 @@
-const basketTemplate = () => {
+const basketTemplate = (name, price, ammount) => {
 	return `
-        <div class="basket-headline-div">
-            <h2 class="basket-headline">Warenkorb</h2>
-        </div>
+<div class="basket-main-div" id="basket-items">
+	<div class="basket-item-div">
+		<p class="basket-item-name">${name}</p>
+		<p class="basket-item-price orange">${price}€</p>
+	</div>
+	<div class="ammount-btn-div">
+		<button onclick="removeAmmount()"><img src="./assets/icon/remove.svg" alt="" /></button>
+		<button onclick="addAmmount()"><img src="./assets/icon/plus.svg" alt="" /></button>
+	</div>
+	<div class="basket-amout-div">
+		<p class="basket-item-ammount">${ammount}</p>
+	</div>
+</div>
+
     `;
 };
 
@@ -12,7 +23,7 @@ const dishTemplate = (name, price, description) => {
 	<div class="inner-dish">
 		<h3>${name}</h3>
 		<p>${description}</p>
-		<p>${price}€</p>
+		<p class="orange">${price}€</p>
 	</div>
 	<div class="add-btn-div">
 		<button class="add-btn">
